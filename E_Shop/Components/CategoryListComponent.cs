0e8877498f3436067130aa3c41c2467bd4c834ee
Artem <ariyan>
2022-08-22 21:08:56 +0430

    'CreateViewComponent'

---
+++ E_Shop/Components/CategoryListComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using E_Shop.Data;
+using System.Threading.Tasks;
+
+namespace E_Shop.Components
+{
+    public class CategoryListComponent : ViewComponent
+    {
+        private EShopContext _context;
+
+        public CategoryListComponent(EShopContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var category = _context.Categories;
+
+            return View("/Views/Component/CategoryListComponentView.cshtml", _context.Categories);
+        }
+    }
+}
