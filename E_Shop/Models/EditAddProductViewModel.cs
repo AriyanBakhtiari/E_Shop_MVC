94b711bb339b4673a1eb71e419066f102a81ec39
Artem <ariyan>
2022-08-29 22:30:01 +0430

    Create Add Product Page

---
+++ E_Shop/Models/EditAddProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace E_Shop.Models
+{
+    public class EditAddProductViewModel
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int QuantityInStock { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
--- E_Shop/Pages/Admin/Add.cshtml.cs
+++ E_Shop/Pages/Admin/Add.cshtml.cs
@@ -1,12 +1,65 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using E_Shop.Models;
+using E_Shop.Data;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
 
 namespace E_Shop.Pages.Admin
 {
     public class AddModel : PageModel
     {
+        private EShopContext _context;
+        public AddModel(EShopContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public EditAddProductViewModel Product { get; set; }
         public void OnGet()
         {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            };
+            var item = new Item
+            {
+                Price = Product.Price,
+                QuantityInStock = Product.QuantityInStock,
+            };
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            var pro = new Product
+            {
+                Name = Product.Name,
+                Description = Product.Description,
+                Item = item,
+            };
+            _context.Products.Add(pro);
+            _context.SaveChanges();
+            pro.ItemId = pro.Id;
+            _context.SaveChanges();
+
+            if (Product.Image?.Length > 0)
+            {
+                string Location = Path.Combine(Directory.GetCurrentDirectory(), 
+                    "wwwroot"
+                    , "Images"
+                    , pro.Id + Path.GetExtension(Product.Image.FileName));
+                using (var stream = new FileStream(Location,FileMode.Create))
+                {
+                    Product.Image.CopyTo(stream);
+                }
+            }
+
+
+
+            return RedirectToPage("index");
         }
     }
 }
