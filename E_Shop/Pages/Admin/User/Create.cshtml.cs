15fa9a10008a693aeb378af1097a1213c2c5b3d2
Artem <ariyan>
2022-09-03 12:11:44 +0430

    create User Control Page

---
+++ E_Shop/Pages/Admin/User/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using E_Shop.Data;
+using E_Shop.Models;
+
+namespace E_Shop.Pages.Admin.ManageUsers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly E_Shop.Data.EShopContext _context;
+
+        public CreateModel(E_Shop.Data.EShopContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Users Users { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Users.Add(Users);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ E_Shop/Pages/Admin/User/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using E_Shop.Data;
+using E_Shop.Models;
+
+namespace E_Shop.Pages.Admin.ManageUsers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly E_Shop.Data.EShopContext _context;
+
+        public IndexModel(E_Shop.Data.EShopContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Users> Users { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Users = await _context.Users.ToListAsync();
+        }
+    }
+}
